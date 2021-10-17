76b76cb274bd4eea160ca2a294e7e1da320e7c44
Alexander <26022428+xakep71k@users.noreply.github.com>
2021-10-17 22:09:29 +0300

    .

---
+++ simple/rust/part8/src/interpreter.rs
@@ -0,0 +1,26 @@
+use crate::parser::{Parser, AST};
+use crate::token::Type;
+
+pub fn interpret(parser: Parser) -> Result<i32, String> {
+    let tree = parser.parse()?;
+    // println!("{:?}", tree);
+    Ok(visit_node(tree))
+}
+
+fn visit_node(node: AST) -> i32 {
+    match node {
+        AST::Num { token } => token.value(),
+        AST::BinOp { left, right, op } => match op.kind() {
+            Type::MUL => visit_node(*left) * visit_node(*right),
+            Type::DIV => visit_node(*left) / visit_node(*right),
+            Type::PLUS => visit_node(*left) + visit_node(*right),
+            Type::MINUS => visit_node(*left) - visit_node(*right),
+            any => panic!("impossible kind {:?}", any),
+        },
+        AST::UnaryOp { op, expr } => match op.kind() {
+            Type::PLUS => visit_node(*expr),
+            Type::MINUS => -visit_node(*expr),
+            any => panic!("impossible kind {:?}", any),
+        },
+    }
+}
