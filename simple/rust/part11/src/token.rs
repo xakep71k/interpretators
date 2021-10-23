0aa2abbe429b889a3619af8ea9a4c0ed447137e9
Alexander <26022428+xakep71k@users.noreply.github.com>
2021-10-23 15:50:34 +0300

    .

---
+++ simple/rust/part11/src/token.rs
@@ -0,0 +1,30 @@
+use crate::var_type::VarType;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Kind {
+    #[allow(non_camel_case_types)]
+    INTEGER_CONST(i32),
+    #[allow(non_camel_case_types)]
+    REAL_CONST(f32),
+    PLUS,
+    MINUS,
+    MUL,
+    COMMA,
+    COLON,
+    LPAREN,
+    RPAREN,
+    ID(String),
+    ASSIGN,
+    BEGIN,
+    PROGRAM,
+    VAR,
+    #[allow(non_camel_case_types)]
+    INTEGER_DIV,
+    #[allow(non_camel_case_types)]
+    FLOAT_DIV,
+    TYPE(VarType),
+    END,
+    SEMI,
+    DOT,
+    EOF,
+}
