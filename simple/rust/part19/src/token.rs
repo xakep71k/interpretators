3010ca56a90ec4190e7ad014b2bc49a55e5a52e3
Alexander <26022428+xakep71k@users.noreply.github.com>
2021-10-30 16:14:49 +0300

    .

---
+++ simple/rust/part19/src/token.rs
@@ -0,0 +1,56 @@
+use crate::var_type::VarType;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Kind {
+    #[allow(non_camel_case_types)]
+    INTEGER_CONST(i32),
+    #[allow(non_camel_case_types)]
+    REAL_CONST(f32),
+    PLUS,
+    MINUS,
+    MUL,
+    COMMA,
+    COLON,
+    LPAREN,
+    RPAREN,
+    ID(String),
+    ASSIGN,
+    BEGIN,
+    PROGRAM,
+    VAR,
+    #[allow(non_camel_case_types)]
+    INTEGER_DIV,
+    #[allow(non_camel_case_types)]
+    FLOAT_DIV,
+    TYPE(VarType),
+    END,
+    SEMI,
+    DOT,
+    EOF,
+    PROCEDURE,
+}
+
+impl std::fmt::Display for Kind {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(fmt, "{:?}", self)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token {
+    pub kind: Kind,
+    pub lineno: usize,
+    pub column: usize,
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            fmt,
+            "Token({}, position={}:{})",
+            self.kind, self.lineno, self.column
+        )?;
+        Ok(())
+    }
+}
